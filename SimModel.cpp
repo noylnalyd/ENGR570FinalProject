e33cd2ad2903b8571e9c80598533cd2d2efde0dd
Dylan Lyon <dylanscottlyon@gmail.com>
2022-11-10 17:03:28 -0500

    Added constants values. Added BC decalration.

--- SimModel.cpp
+++ SimModel.cpp
@@ -26,16 +26,26 @@ namespace SIMMODEL
         const double salineCp; // J/kg/K, saline specific heat capacity
 
         // Environmental constants
-        const double Pair; // Pa, air pressure
-        const double vAirEff; // m/s, effective air velocity
+        const double Pair = 2300; // Pa, partial vapor air pressure
+        const double vAirEff = 0.05; // m/s, effective air velocity
+        const double echelonSrOutdoors = 0.46; // -
+        const double echelonSrIndoors = 0.93; // -
+        const double alphaSrOutdoors = 0.3; // -
+        const double alphaSrIndoors = 0.8; // -
+        const double sOutdoors = 10752/126.7; // W/m^2
+        const double sIndoors = 500/50; // W/m^2
+        const double Tmelt = 273.15; // K, melting point of water
 
-        // UQ attributes worth exploring
+        // UQ attributes worth exploring for all cases
         double TairIndoors; // K, inside / treatment air temperature
         double TairOutdoors; // K, outside / pretreatment air temperature
         double TsrmIndoors; // K, mean surroundings temperature inside
         double TsrmOutdoors; // K, mean surroundings temperature outside
 
+        virtual void BC( int element, int sector, int washer, double time, double* T, double* Trhs)=0;
     };
+
+    
 
 
 }
