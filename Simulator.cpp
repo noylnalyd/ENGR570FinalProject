c35a7a32aca4e0ee28c1bedb8481915ccb6bb5ed
NoylNalyd <dylanscottlyon@gmail.com>
2022-11-22 07:24:36 -0500

    Added to simulator code skeleton

--- Simulator.cpp
+++ Simulator.cpp
@@ -8,9 +8,9 @@ namespace SIMULATOR
 
     void Simulator::preallocate(){
         assert(_state == SIMULATOR::simLoaded);
-        
-namespace SIMULATOR{
-    void Simulator::computeThermalLoadParameters(double* M, double* H, double*Q)
+
+    }
+    void Simulator::computeThermalLoadParameters()
     {
         double Mbas0 = 0; // W
         double MbasDelta = 0; // W
@@ -19,17 +19,85 @@ namespace SIMULATOR{
         for(int eleIdx = 0; eleIdx < body->nElements; ++eleIdx){
             ELEMENT::Element* elem = body->elements[eleIdx];
             WASHER::Washer* cur = elem->washers[0];
-            Mbas0 += cur
+            Mbas0 += cur->volume*elem->sumPhi/(2*M_PI)*cur->q_m;
+            MbasDelta += cur->volume*elem->sumPhi/(2*M_PI)*sim->thermoneutral*deltaQMetabolic(cur->q_m,T[Tidx],Tn[Tidx],sim->Q10);
+            Tidx++;
+            for(int secIdx = 0; secIdx<elem->nSectors; ++secIdx){
+                SECTOR::Sector* sect = elem->sectors[secIdx];
+                for(int washIdx = 1; washIdx<elem->nWashers; ++washIdx){
+                    cur = elem->washers[washIdx];
+                    Mbas0 += cur->volume*sect->phi/(2*M_PI)*cur->q_m;
+                    MbasDelta += cur->volume*sect->phi/(2*M_PI)*sim->thermoneutral*deltaQMetabolic(cur->q_m,T[Tidx],Tn[Tidx],sim->Q10);
+                    Tidx++;
+                }
+            }
         }
+        double act = 1 + (Sh+MbasDelta)/Mbas0*MperWork; // MET
+        double etaW;
+        if(act < 1.6)
+            etaW = 0.05; // W/W
+        else
+            etaW = 0.2*tanh(body->b1*act+body->b0); // W/W
+        M = act*Mbas0/actBas; // W
+        H = M*(1-etaW)-Mbas0; // W
+        Qresp = computeQresp(); // W
+
     }
-}    }
+    double computeQresp(){
+        return 3.45*M*(0.028+6.5e-5*sim->Tair-4.98e-6*sim->Pair)+
+            1.44e-3*M*(32.6-0.934*sim->Tair+1.99e-4*sim->Pair);
+    }
+    double deltaQMetabolic( double q, double T, double Tn, double Q10 )
+    {
+        return q*(pow(Q10,(T-Tn)/10.0)-1);
+    }
+
     void Simulator::runSim(){
         
         // Check status of body and simmodel. Make sure both completed.
         assert(body->getState() == BODYMODEL::staticPBMAssembled);
         assert(sim->getState() == SIMMODEL::allValuesAssigned);
 
-        
+        // Declare local temporary values
+        ELEMENT::Element* element;
+        SECTOR::Sector* sector;
+        WASHER::Washer* washer;
+
+        // Provide initial values
+
+        // Iterate
+        for(int timestep=1;timestep<nSteps;++timestep){
+            
+            // Compute temporary BC values and properties from sim
+
+            // Skin handling, Brain handling
+
+            // Compute error inputs
+
+            // Compute active controls
+
+            // Compute whole-body thermal load parameters
+
+            // Project to get future values
+
+            // Empty PBM
+
+            // Loop thru elements
+            for(int eleIdx = 0; eleIdx < body->nElements; ++eleIdx){
+                element = body->elements[eleIdx];
+                // Compute element-level blood pool properties
+                
+                // Initialize core node values, including cusp-less projection
+
+                // Loop thru sectors
+                for(int sectIdx = 0; sectIdx < elem->nSectors; ++eleIdx){
+                    sector = element->sectors[sectIdx];
+                    // Loop thru washers
+                }
+
+            }
+
+        }
 
     }
 }
--- Simulator.hpp
+++ Simulator.hpp
@@ -31,17 +31,70 @@ namespace SIMULATOR
         SIMMODEL::SimModel* sim; // Simulator options object
 
 
-        // Values to record over sym
-        double* Tskm; // K, Mean skin temperature
-        double* Thy; // K, Hypothalamic temperature
-        double* Tblp; // K, Blood pool temperature
-        double* H; // W, Heat load
+        // Values used in whole simulation.
+        double Tskm; // K, Mean skin temperature
+        double Thy; // K, Hypothalamic temperature
+        double Tblp; // K, Blood pool temperature
+        double M; // W, Total metabolism
+        double H; // W, Heat load
+        double Qresp; // W, Respiratory cooling intake
+        // Active controls:
+        double Sh; // W, Shivering power
+        double Cs; // -, Vasoconstriction ratio
+        double Dl; // W/K, Vasodilation capacitance
+        double Sw; // g/min, Sweat output
 
-        // Active controls, also recorded
-        double* Sh; // W, Shivering power
-        double* Cs; // -, Vasoconstriction ratio
-        double* Dl; // W/K, Vasodilation capacitance
-        double* Sw; // g/min, Sweat output
+        // Holdover past values
+        double TskmPrv; // K, Mean skin temperature
+        double ThyPrv; // K, Hypothalamic temperature
+        double TblpPrv; // K, Blood pool temperature
+        double MPrv; // W, Total metabolism
+        double HPrv; // W, Heat load
+        double QrespPrv; // W, Respiratory cooling intake
+        // Active controls:
+        double ShPrv; // W, Shivering power
+        double CsPrv; // -, Vasoconstriction ratio
+        double DlPrv; // W/K, Vasodilation capacitance
+        double SwPrv; // g/min, Sweat output
+
+        // Projected future values
+        double TskmNxt; // K, Mean skin temperature
+        double ThyNxt; // K, Hypothalamic temperature
+        double TblpNxt; // K, Blood pool temperature
+        double MNxt; // W, Total metabolism
+        double HNxt; // W, Heat load
+        double QrespNxt; // W, Respiratory cooling intake
+        // Active controls:
+        double ShNxt; // W, Shivering power
+        double CsNxt; // -, Vasoconstriction ratio
+        double DlNxt; // W/K, Vasodilation capacitance
+        double SwNxt; // g/min, Sweat output
+
+        // Useful values, past and present
+        double* T; // K
+        double* T0; // K
+        double* Tprv; // K
+        double Mprv; // W
+        double* QRespprv, // W
+        double* Tskprv, // K
+        double* qprv, // W/m^3
+        double* betaprv, // K
+        double* Tppprv, // K
+        double* Tprv, // K
+        double* Tprv, // K
+        double* Tprv, // K
+        double* Tprv, // K
+        double* Tprv, // K
+
+        // Values to record over sim
+        double* HistTskm; // K, Mean skin temperature
+        double* HistThy; // K, Hypothalamic temperature
+        double* HistTblp; // K, Blood pool temperature
+        double* HistH; // W, Heat load
+        double* HistSh; // W, Shivering power
+        double* HistCs; // -, Vasoconstriction ratio
+        double* HistDl; // W/K, Vasodilation capacitance
+        double* HistSw; // g/min, Sweat output
 
         void preallocate(
             double* Tprv, // K
