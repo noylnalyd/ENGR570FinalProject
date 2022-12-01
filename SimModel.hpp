6d85c08daf006d42104f3f763f1d013554964716
NoylNalyd <dylanscottlyon@gmail.com>
2022-12-01 10:37:49 -0500

    Adding simmodel's skinHeat default

--- SimModel.hpp
+++ SimModel.hpp
@@ -48,13 +48,102 @@ namespace SIMMODEL
         double TsrmIndoors; // K, mean surroundings temperature inside
         double TsrmOutdoors; // K, mean surroundings temperature outside
 
+        // BC values
         SimModelState getState();
-        virtual void skinBC( int element, int sector, double time, double* T, double* Amatrixii, double* Trhs)=0;
+        void skinHeat(BODYMODEL::BodyModel* body,
+            int elemIdx,
+            int sectIdx,
+            double Twash, // K, outermost node temperature
+            double Sw, // g/min, sweat command
+            double TsfAssumed, // K, assumed skin surface temperature
+            int njActual // -, number of equivalent clothings on this sector
+            );
+        virtual void skinBC( int element, int sector, double time, double* T, double* Amatrixii, double* Trhs);
+        virtual void BVRSVR( double *bvr, double *svr, double time );
     };
+
+    void SimModel::BVRSVR( double *bvr, double *svr, double time )
+    {
+        *bvr = 1.0;
+        *svr = 1.0;
+    }
+    // Default heat flux of the skin at a select elment and sector at a particular moment in time
+    double SimModel::skinHeat(
+        BODYMODEL::BodyModel* body,
+        ELEMENT::Element* elem,
+        SECTOR::Sector* sect,
+        int elemIdx,
+        int sectIdx,
+        double Sw, // g/min, sweat command
+        double TsfAssumed, // K, assumed skin surface temperature
+        double Tsf0, // K, skin thermoneutral temperature
+        int njActual, // -, number of equivalent clothings on this sector
+        double psiActual // rad^2, exposure angle
+        ){
+        
+        double qsk = 0, qC = 0, qR = 0, qsR = 0, qe = 0;
+        
+        double f = 1;
+        double echelonSf = body->elements[elemIdx]->sectors[sectIdx]->echelon;
+
+        if(njActual > 0){
+            f = 0.9;
+            echelonSr = 1.0;
+        }
+
+        double TsfAbs = TsfAssumed; // K
+        double TsrmAbs = Tsrm; // K
+        double TsfC = TsfAssumed-273.15; // C
+        double TsrmC = TsrmAbs - 273.15; // C
+        double TairAbs = Tair; // K
+        double TairC = TairC; // C
+
+        double hcmix = sqrt(abs(
+                elem->a_nat*sqrt(abs(TsfC-TairC))
+                +elem->a_frc*vAirEff
+                +elem->a_mix
+        )); // W/m^2/K
+
+        double hR = body->StefanBoltzmann*echelonSf*echelonSrm
+                *(TsfAbs*TsfAbs + TsrmAbs*TsrmAbs)
+                *(TsfAbs + TsrmAbs); // W/m^2/K
+        
+        double U = 1/(njActual*body->Icl+1/(f*(hcmix+hR))); // W/m^2/K
+
+        qsR = alphaSr*psi*s;
+
+        double Tweight = (hcmix*TairC + hR*(TsrmC) + qsR)/(hcmix + hR);
+        double Posksat = 100.0*exp(18.956-4030.0/(TsfC+235));
+        
+        double Ue = body->LewisConstant/(
+            njActual*body->Icl/body->icl
+            +1/(f*hcmix)
+        );
+        double dsweat = elem->a_sw*sect->areaShare*Sw*pow(2,(TsfAssumed-Tsf0)/Q10) * 1e-3/60;
+        double Psk = (body->LambdaH20*dsweat + Posksat*body->Rinverse + Ue*Pair)/
+                (Ue + body->Rinverse);
+        
+        return U*(TsfC-Tweight) + Ue*(Psk-Pair);
+    }
 
     SimModelState SimModel::getState()
     {
         return _state;
+    }
+    class InitialCase : SimModel
+    {
+        thermovariant = 0;
+        transient = 0;
+
+    }
+    class SteadyCase : SimModel
+    {
+        thermovariant = 0;
+        transient = 1;
+    }
+    class TransientCase : SimModel
+    {
+
     }
     class KonstasCase : SimModel
     {
