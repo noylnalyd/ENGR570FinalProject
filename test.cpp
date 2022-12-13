14d9ac9da07bde3185a60461be27345206293184
tcoonsUM <110847410+tcoonsUM@users.noreply.github.com>
2022-12-13 14:26:19 -0500

    Update test.cpp to take command line arguments
    test.cpp now takes command line arguments for easier use with UQ code

--- test.cpp
+++ test.cpp
@@ -5,12 +5,12 @@
 #include <unistd.h>
 #endif
 #include <chrono>
-int main(void){
+int main(int argc, char** argv){
     double *args = new double[4];
-    args[0] = 20+273.15; // TsrmIndoors, indoor surroundings surface temp (K)
-    args[1] = 20+273.15; //TsrmOutdoors, outdoor surroundings surface temp (K)
-    args[2] = 300; // trecovery, time until arrival of ambulance (seconds)
-    args[3] = 7+273.15; // TECMO, temp of of blood-saline mixture from machine (K)
+    args[0] = atof(argv[1]); //20+273.15; // TsrmIndoors, indoor surroundings surface temp (K)
+    args[1] = atof(argv[2]); //20+273.15; //TsrmOutdoors, outdoor surroundings surface temp (K)
+    args[2] = atof(argv[3]); //300; // trecovery, time until arrival of ambulance (seconds)
+    args[3] = atof(argv[4]); //7+273.15; // TECMO, temp of of blood-saline mixture from machine (K)
     double *outs = new double[1];
 
     BODYMODEL::BodyModel* body = BODYMODEL::defaultBody();
