52f0c26aaab2e29c03b2e6e6f1040d9e4037a902
NoylNalyd <dylanscottlyon@gmail.com>
2022-12-13 21:53:37 -0500

    Doxygen'd Sector.hpp

--- sector.hpp
+++ sector.hpp
@@ -7,15 +7,16 @@
 
 namespace SECTOR
 {
+    /// @brief Angular sector class
     class Sector
     {
     public:
-        double phi; // rad, interior angle of sector
-        double psiSed; // -, sedentary view factor
-        double psiStnd; // -, standard view factor
-        double echelon; // -, emission coefficient
-        double areaSkin; // m^2, skin surface area
-        double areaShare; // -, skin share proportion
+        double phi; //!< rad, interior angle of sector
+        double psiSed; //!< -, sedentary view factor
+        double psiStnd; //!< -, standard view factor
+        double echelon; //!< -, emission coefficient
+        double areaSkin; //!< m^2, skin surface area
+        double areaShare; //!< -, skin share proportion
     };
 }
 
